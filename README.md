4f428a42c53fc1df361c4ad2f41f64ae94039f77
Matthew-Hu-cmd <760061861@qq.com>
2023-04-08 00:51:46 +0800

    虽然但是，这样写为什么显存会炸啊

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SAM-COD  
 基于prompt-engineering、one-shot transfer learing将Meta AI的工作*segment-anything*迁移到伪装目标检测的任务中
+```
+ requirements：python = 3.8 torch = 1.8.0 
+ CUDA = 11.1 torchvision = 0.9.0 python-opencv
+ segment-anything source code and pre-trained weights
+ ```
+
+ 并不公平的对比：案例可见```test-cod.ipynb```,是否有必要将第一次的mask再次和gt对比得出遗漏的区域并在用互补的bbox对齐进行预测，mask补全  
--- test_cod.py
+++ test_cod.py
@@ -2,12 +2,62 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import cv2
-from segment_anything import build_sam, SamPredictor 
+import os, argparse
+from segment_anything import sam_model_registry, build_sam, SamPredictor 
 
-class BBox_gen():
-    
+def get_bbox(gt):
+    """
+    Given a ground truth segmentation image, returns a bounding box that completely encloses the segmentation.
+    :param img: A ground truth segmentation image
+    :return: A numpy array of the form [x1, y1, x2, y2] representing the bounding box
+    """
+    # Convert the image to binary
+    _, binary = cv2.threshold(gt, 0, 255, cv2.THRESH_BINARY)    # Find the contours of the binary image
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find the bounding box of the contours
+    x, y, w, h = cv2.boundingRect(contours[0])
+    # Return the bounding box as a numpy array
+    return np.array([x, y, x+w, y+h])
 
 
-predictor = SamPredictor(build_sam(checkpoint="</path/to/model.pth>"))
-predictor.set_image(<your_image>)
-masks, _, _ = predictor.predict(<input_prompts>)
+if __name__ == '__main__':
+    sam_checkpoint = "/root/autodl-tmp/segment-anything-pth/sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+    # 单卡推理
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    sam = sam_model_registry[model_type](checkpoint= sam_checkpoint)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    for _data_name in ['CAMO', 'COD10K', 'CHAMELEON']:  # iterate all datasets
+        data_path = '/root/bachelor-thesis/Dataset/TestDataset/{}/'.format(_data_name)
+        save_path = '/root/autodl-tmp/res/SAM_COD/{}/'.format(_data_name)
+
+        # Load images and corresponding ground truth from data_path/Imgs and data_path/GT respectively
+        img_path = os.path.join(data_path, 'Imgs')
+        gt_path = os.path.join(data_path, 'GT')
+        for img_file in os.listdir(img_path):
+            img = cv2.imread(os.path.join(img_path, img_file))
+            gt_file = img_file.split('.')[0] + '.png'
+            gt = cv2.imread(os.path.join(gt_path, gt_file), cv2.IMREAD_GRAYSCALE)
+            bbox = get_bbox(gt)
+            # print('***load image and gt!***\n')
+            predictor.set_image(img)    # embedding image using SAM
+            mask, _, _ = predictor.predict(point_coords= None,     # 推理，生成Masks
+                                            point_labels= None,
+                                            box = bbox[None, :],
+                                            multimask_output=False)
+            # Save the generated mask as a PNG file
+            mask_path = os.path.join(save_path, img_file.split('.')[0] + '.png')
+            mask_int = (mask * 255).astype(np.uint8)
+            cv2.imwrite(mask_path, mask_int)
+            print('>')
+        print("finish processing dataset:"+_data_name+'\n')
+            
+            
+
+
+
+
+
+
+
